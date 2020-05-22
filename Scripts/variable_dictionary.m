a6873075fd8df49ab3a87b7bbced3f8714415efd
lfc-256 <luisf_cm@outlook.com>
2020-05-21 23:53:10 -0500

    TASK: Create a variable dictionary and values
    This dictionary include all parameters that we need to use in Matlab
    
    Resolves: Issue-9
    
    Signed-off-by: lfc-256 <luisf_cm@outlook.com>

--- Scripts/variable_dictionary.m
+++ Scripts/variable_dictionary.m
@@ -1,5 +1,5 @@
 L1=2.984;   %Length of the car (measured between the two wheel axles)
 L12=1.476;  %Length between the rear axle and the trailer contact point (m)
 L2=1.0;     %Length of the trailer (m)
-v=5.0;      %Speed
+v=5.0;      %Speed  (m/s)
 Df=1;       %Angle between the front wheel and the car  (rad)
