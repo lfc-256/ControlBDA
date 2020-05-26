55134052ec07b0f8ac9afa5414c01404b72b8acd
lfc-256 <luisf_cm@outlook.com>
2020-05-26 13:04:10 -0500

    TASK: Provide better organization of information
    The changes include header of information about the functionality of
    dictionary and a sections for contro parameters/vehicle
    parameters/trailes parameters.
    
    Resolves: better interpretation of information
    
    Signed-off-by: lfc-256 <luisf_cm@outlook.com>

--- Scripts/variable_dictionary.m
+++ Scripts/variable_dictionary.m
@@ -1,5 +1,20 @@
+%{
+This scripts provides a variable dictionary with values of control
+parameters, vehicle parameters and trailer parameters that are used in the
+ssimulation .
+%}
+
+%% Control parameters
+h = 0.0;  %sampling period
+Kp = 0.0; %Proportional gain
+Ki = 0.0; %integral gain   
+Kd = 0.0; %differential gain  
+
+%% Vehicle parameters
 L1=2.984;   %Length of the car (measured between the two wheel axles)
 L12=1.476;  %Length between the rear axle and the trailer contact point (m)
-L2=1.0;     %Length of the trailer (m)
 v=5.0;      %Speed  (m/s)
 Df=1;       %Angle between the front wheel and the car  (rad)
+
+%% Trailer parameters
+L2=1.0;     %Length of the trailer (m)
