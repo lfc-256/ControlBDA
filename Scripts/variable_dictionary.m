83db66dc0c34c0d685f2386a09349cef5a1eb6d3
lfc-256 <luisf_cm@outlook.com>
2020-05-26 15:29:43 -0500

    TASK: Provide better organization of information 2
    The changes include the use if "%%" instead of "%{ %}" to header
    comment.
    
    Resolve: Conflicts files with branch
    
    Signed-off-by: lfc-256 <luisf_cm@outlook.com>

--- Scripts/variable_dictionary.m
+++ Scripts/variable_dictionary.m
@@ -1,20 +1,16 @@
-%{
-This scripts provides a variable dictionary with values of control
-parameters, vehicle parameters and trailer parameters that are used in the
-simulation .
-%}
+%% This scripts provides a variable dictionary with values of control parameters, vehicle parameters and trailer parameters that are used in the simulation.
 
-%% Control parameters
+% Control parameters
 h = 0.0;  %sampling period
 Kp = 0.0; %Proportional gain
 Ki = 0.0; %integral gain   
 Kd = 0.0; %differential gain  
 
-%% Vehicle parameters
+% Vehicle parameters
 L1=2.984;   %Length of the car (measured between the two wheel axles)
 L12=1.476;  %Length between the rear axle and the trailer contact point (m)
 v=5.0;      %Speed  (m/s)
 Df=1;       %Angle between the front wheel and the car  (rad)
 
-%% Trailer parameters
+% Trailer parameters
 L2=1.0;     %Length of the trailer (m)
