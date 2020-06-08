862e53e1aca51939adbef99201066587bf549a67
lfc-256 <luisf_cm@outlook.com>
2020-06-08 16:39:56 -0500

    Variables of vehicle and trailer added with the corrections

--- Scripts/variable_dictionary.m
+++ Scripts/variable_dictionary.m
@@ -5,35 +5,36 @@
 %   and trailer parameters that are loaded and used in the simulation (Simulink).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% No. Manuever
-manue = 0;
+%% Manuever number
+manue = 0;		%kind of manuever that make the car
 
-% Control parameters
-h = 0.0;    %sampling period
-Kp = 0.0;   %Proportional gain
-Ki = 0.0;   %integral gain   
-Kd = 0.0;   %differential gain  
+%% Control parameters
+h = 0.0;    		%sampling period
+Kp = 0.0;   		%Proportional gain
+Ki = 0.0;   		%integral gain   
+Kd = 0.0;   		%differential gain  
 
-% Vehicle parameters
-L1=2.984;   %Length of the car (measured between the two wheel axles)
-L12=1.476;  %Length between the rear axle and the trailer contact point (m)
-v=5.0;      %Speed  (m/s)
-Df=1;       %Angle between the front wheel and the car  (rad)
+%% Vehicle parameters
+L1=2.984;   		%Length of the car (measured between the two wheel axles) (m)
+L12=1.476;  		%Length between the rear axle and the trailer contact point (m)
+v=5.0;      		%Speed  (m/s)
+Df=1;       		%Angle between the front wheel and the car  (rad)
+m1= 2800;        	%mass of the vehicle (kg)
+Iz1= m1*L^2/12;         %yaw moments of inertia of the vehicle (kg*m)
+a1=1.587754;       	%distance of the vehicle front to the center of mass (m)
+b1=1.396746;       	%distance of the vehicle rear to the center of mass  (m)
+e1=L12;        		%distance of the rear axle of the vehicle to the hitch point (m)
+c1=b1+e1;      		%distance of the hitch point to the vehicle center of mass (m)
+Cyf=1000;           	%cornering stiffness values for the tires of the vehicle’s front axle
+Cyr=1000;           	%cornering stiffness values for the tires of the vehicle’s rear axle
+vx=v;               	%speed (m/s)
 
-%Vehicle parameters 2
-m1= 0;        %mass of the vehicle
-Iz1= 0;          %yaw moments of inertia of the vehicle
-a1=L1/2;       %distance of the vehicle front to the center of mass
-b1=L1/2;       %distance of the vehicle rear to the center of mass
-e1=L12;        %distance of the rear axle of the vehicle to the hitch point 
-c1=b1+e1;      %distance of the hitch point to the vehicle center of mass 
-
-
-% Trailer parameters
-L2=1.0;     %Length of the trailer (m)
-m2=  0;           %mass of the  vehicle
-Iz2=   0;         %yaw moments of inertia of the trailer
-a2=L2/2;        %distance of the trailer center of mass to the hitch point
-b2=L2/2;        %distance of the trailer center of mass to the trailer axle
-l2=a2+b2;       %distance of the trailer axle to the hitch point
+%% Trailer parameters
+L2=1.8288;     		%Length of the trailer (m)
+m2=205;           	%mass of the  vehicle  (kg)
+Iz2=m2*L2^2/12;         %yaw moments of inertia of the trailer (kg*m)
+a2=L2/2;        	%distance of the trailer center of mass to the hitch point (m)
+b2=L2/2;        	%distance of the trailer center of mass to the trailer axle (m)
+l2=a2+b2;       	%distance of the trailer axle to the hitch point (m)
+Cyt=1000;           	%cornering stiffness values for the tires of the trailer axle
 
